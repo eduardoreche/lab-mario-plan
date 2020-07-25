cb12ffe731715392fa50c81be5c9b7e4bfbaee6c
Eduardo Reche <eduardo@reche-mac2.lan>
2020-07-25 20:19:10 -0300

    setup firebase

---
+++ src/config/fbConfig.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBSzbNzlXeJFvd_XF4Sh1uMCg511_4T7g4",
+    authDomain: "lab-mario-plan.firebaseapp.com",
+    databaseURL: "https://lab-mario-plan.firebaseio.com",
+    projectId: "lab-mario-plan",
+    storageBucket: "lab-mario-plan.appspot.com",
+    messagingSenderId: "34333493605",
+    appId: "1:34333493605:web:b32e6bc31b8e25e153b98c",
+    measurementId: "G-KRLPBL7B1G"
+  };
+  
+  firebase.initializeApp(firebaseConfig);
+  firebase.firestore().settings({ timestampsInSnapshots: true });
+  firebase.analytics();
+
+  export default firebase;
