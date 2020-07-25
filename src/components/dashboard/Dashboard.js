df03d51c91714007d9f7ac4e99e46d5c2e70db45
Eduardo Reche <eduardo@reche-mac2.lan>
2020-07-25 19:52:49 -0300

    connect redux store to project list

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,14 +1,17 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 import Notifications from './Notifications'
 import ProjectList from '../projects/ProjectList'
 
 const Dashboard = () => {
+    const { projects } = useSelector(({project}) => project);
+
     return (
         <div className="dashboard container">
             <div className="row">
                 <div className="col s12 m6">
-                    <ProjectList />
+                    <ProjectList projects={projects} />
                 </div>
                 <div className="col s12 m5 offset-m1">
                     <Notifications />
---
+++ src/store/reducers/projectReducer.js
@@ -0,0 +1,14 @@
+const initState = {
+    projects: [
+        {id: 1, title: 'Test', content: 'lorem '},
+        {id: 2, title: 'Project 2', content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas nemo dolorem neque! Dolores, magnam illum quam vero praesentium ipsum cum obcaecati asperiores quo dolore saepe quae itaque fugiat libero amet!'},
+        {id: 3, title: 'Mario Plan', content: 'study '},
+        {id: 4, title: 'TestMario Plan', content: 'study '},
+    ]
+};
+
+const projectReducer = (state = initState, action) => {
+    return state;
+}
+
+export default projectReducer;
