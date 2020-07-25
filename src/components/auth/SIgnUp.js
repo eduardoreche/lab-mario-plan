0436a0bd35d1ce951568d61558c672b5ec2e329b
Eduardo Reche <eduardo@reche-mac2.lan>
2020-07-25 19:11:44 -0300

    signin signup comp

---
+++ src/components/auth/SIgnUp.js
@@ -0,0 +1,55 @@
+import React, {useState} from 'react'
+
+const SignUp = () => {
+    const [state, setState] = useState({
+        email: '',
+        password: '',
+        firstName: '',
+        lastName: '',
+    });
+
+    const handleChange = (e) => {
+        setState({
+            ...state,
+            [e.target.id]: e.target.value,
+        });
+    }
+    
+    const handleSubmit = (e) => {
+        console.log(state);
+        e.preventDefault();
+    }
+
+    return (
+        <div className="container">
+            <form onSubmit={handleSubmit} className="white">
+                <h5 className="grey-text text-darken-3">Sign Up</h5>
+                <div className="input-field">
+                    <label htmlFor="email">Email</label>
+                    <input type="email" id="email" onChange={handleChange}/>
+                </div>
+
+                <div className="input-field">
+                    <label htmlFor="password">Password</label>
+                    <input type="password" id="password" onChange={handleChange}/>
+                </div>
+
+                <div className="input-field">
+                    <label htmlFor="firstName">First Name</label>
+                    <input type="text" id="firstName" onChange={handleChange}/>
+                </div>
+
+                <div className="input-field">
+                    <label htmlFor="lastName">Last Name</label>
+                    <input type="text" id="lastName" onChange={handleChange}/>
+                </div>
+
+                <div className="input-field">
+                    <button className="btn pink lighten-1 z-depth-0">Sign up</button>
+                </div>
+            </form>
+        </div>
+    )
+}
+
+export default SignUp
